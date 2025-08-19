64d1c4548ec1b32dbbdb15c9255f9575fcaa6f93
Ana Luiza <analu.tibi@outlook.com>
2025-08-19 09:15:50 -0300

    3. Começando TSX

--- blogPessoal/blogP/src/pages/home/home.tsx
+++ blogPessoal/blogP/src/pages/home/home.tsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 function home() {
   return (
-    <div>home</div>
-  )
+    <>
+    <div style={{
+        width:"100vw",
+        display:"flex",
+        justifyContent:"center"
+    }}></div>
+    <div>
+        <div style={{
+            width:"80vw",
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center"
+        }}>
+            <h2>Seja bem vinda</h2>
+            <p>Expresse aqui seus pensamentos e opiniões :D</p>
+        </div>
+        <div style={{
+            width:"80vw",
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center"
+
+            <img src="../../assets/imgHome.jpg" alt="filhote de cachorro" />
+        }}></div>
+    </div>
+    </>)
 }
 
 export default home
